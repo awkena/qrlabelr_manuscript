313f84a7932904649a599bd271d177cb80fe8402
Alexander Wireko Kena <alex.kena24@gmail.com>
2023-12-21 19:54:39 +0000

    plot rotated

--- Hypothesis testing for qrlabelr.R
+++ Hypothesis testing for qrlabelr.R
@@ -116,11 +116,12 @@ str(res_win)
 summ_mac <- res_mac |> dplyr::group_by(package) |> dplyr::summarise(min = signif(min(milli.time), 3),
                                                             mean = signif(mean(milli.time), 3),
                                                             max = signif(max(milli.time), 3),
-                                                           st.dev = signif(sd(milli.time), 3))
+                                                           st.dev = signif(sd(milli.time), 1))
+
 summ_win <- res_win |> dplyr::group_by(package) |> dplyr::summarise(min = signif(min(milli.time), 3),
                                                                     mean = signif(mean(milli.time), 3),
                                                                     max = signif(max(milli.time), 3),
-                                                                    st.dev = signif(sd(milli.time), 3))
+                                                                    st.dev = signif(sd(milli.time), 2))
 # View summarized results
 
 # View summarized results
@@ -139,7 +140,8 @@ summ_win[,-c(1,5)][2,]/summ_win[,-c(1, 5)][1,] # On PC
 ttest_mac <- t.test(milli.time ~ package, data = res_mac, alternative = "less", 
                 var.equal = FALSE, paired = FALSE)
 
-gtext_mac <- paste(paste0('t-statistic = ', round(ttest_mac$statistic, 2), ';'), 
+gtext_mac <- paste(paste0('t-statistic = ', round(ttest_mac$statistic, 2), ';'),
+                   paste0('df = ', round(ttest_mac$parameter, 1), ';'),
                'p value =', signif(ttest_mac$p.value, 2))
 gtext_mac
 
@@ -148,6 +150,7 @@ ttest_win <- t.test(milli.time ~ package, data = res_win, alternative = "less",
                     var.equal = FALSE, paired = FALSE)
 
 gtext_win <- paste(paste0('t-statistic = ', round(ttest_win$statistic, 2), ';'), 
+                   paste0('df = ', round(ttest_win$parameter, 1), ';'),
                    'p value =', signif(ttest_win$p.value, 2))
 gtext_win
 ################################################################################
@@ -168,75 +171,60 @@ library(ggpmisc)
 
 
 # Set y axis (Time axis) limits
-y_min <- 0
+y_min <- 1
 
 ################################################################################
 # For MacBook data
 # Set upper limit of the y axis to 5 percent more than the maximum value
-y_max_m <- 1.05*max(res_mac$milli.time)
 
 # Make the plot for MacBook data
 pp <- ggplot(res_mac, aes(x = package, y = milli.time, color = package)) 
       
-
-pp <- pp + coord_cartesian(ylim = c(y_min, y_max_m))
+# Log transform Time axis 
+pp <- pp + scale_y_log10(name = "Time [milliseconds]")
 
 pp <- pp + stat_ydensity(linewidth = .8) + 
       geom_boxplot(width = 0.1, fill = "white", linewidth = .5)
 
-
 pp <- pp + scale_x_discrete(name = "Package")
-
-# Log transform Time axis 
-pp <- pp + scale_y_log10(name = "Time [milliseconds]")
-
-# Flip axis
-pp <- pp + ggplot2::coord_flip()
 
 # Change to classical theme
 pp <- pp + theme_classic()
 
-pp <- pp + geom_text(aes(label = hypothesis, y = 2500, x = 1.4), color = "blue") 
-pp <- pp + geom_text(aes(label = gtext_mac, y = 3000, x = 1.3), color = "blue") 
+pp <- pp +  annotate("table_npc", npcx = "center", npcy = "center",
+               label = list(summ_mac), size = 5)
+pp <- pp + theme(axis.title.y = element_blank(),
+                 text = element_text(size = 18, color = 'black'))
+                 
 
-pp <- pp +  annotate("table_npc", npcx = "center", npcy = "bottom",
-               label = list(summ_mac))
-pp <- pp + theme(axis.title.y = element_blank())
 pp
+
+
 ################################################################################
 
 ################################################################################
 # Plot for Windows PC data
-# Set upper limit of the y axis to 5 percent more than the maximum value
-y_max_w <- 1.05*max(res_win$milli.time)
 
 # Make the plot for MacBook data
 ppp <- ggplot(res_win, aes(x = package, y = milli.time, color = package)) 
 
-
-ppp <- ppp + coord_cartesian(ylim = c(y_min, y_max_w))
-
 ppp <- ppp + stat_ydensity(linewidth = .8) + 
   geom_boxplot(width = 0.1, fill = "white", linewidth = .5)
-
 
 ppp <- ppp + scale_x_discrete(name = "Package")
 
 # Log transform Time axis 
 ppp <- ppp + scale_y_log10(name = "Time [milliseconds]")
 
-# Flip axis
-ppp <- ppp + ggplot2::coord_flip()
-
 # Change to classical theme
 ppp <- ppp + theme_classic()
 
-ppp <- ppp + geom_text(aes(label = hypothesis, y = 10000, x = 1.4), color = "blue") 
-ppp <- ppp + geom_text(aes(label = gtext_win, y = 10000, x = 1.3), color = "blue") 
+# ppp <- ppp + geom_text(aes(label = hypothesis, y = 10000, x = 1.4), color = "blue") 
+# ppp <- ppp + geom_text(aes(label = gtext_win, y = 10000, x = 1.3), color = "blue") 
 
-ppp <- ppp +  annotate("table_npc", npcx = "center", npcy = "bottom",
-               label = list(summ_win))
-ppp <- ppp + theme(legend.position = 'none')
+ppp <- ppp +  annotate("table_npc", npcx = "center", npcy = "center",
+               label = list(summ_win), size = 5)
+ppp <- ppp + theme(text = element_text(size = 18, color = 'black'))
 ppp
 ################################################################################
 
@@ -253,7 +241,7 @@ plts <- list(ppp, pp)
 ggsave(filename = paste0("violin_plots",  ".pdf"), 
        plot = marrangeGrob(plts, nrow = 1, ncol = 2), 
        device = "pdf", path = getwd(), units = "in",
-       width = 10.5, height = 5, onefile = TRUE)
+       width = 13.5, height = 5, onefile = TRUE)
 
 
 
